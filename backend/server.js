b7bd81848ffc725beb6ecbf78ab1c615760b1c90
TLyncks <tairose@protonmail.com>
2025-03-31 15:24:53 +0100

    updated server.js to connect together_db database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("require");
 require("dotenv").config();
+const db = require("../config/db");
 
 const app = express();
 app.use(cors());
