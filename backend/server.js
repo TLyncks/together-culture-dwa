59451f279bff52d0a77c634d4de9764be7132536
TLyncks <tairose@protonmail.com>
2025-03-31 14:59:09 +0100

    server.js set-up and port validation process

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("require");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API is live"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
